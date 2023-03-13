c55d591ea42da0357366aaba9b5815ae86e7f12a
mleleszi@icloud.com <mleleszi@icloud.com>
2023-03-13 10:08:31 +0000

    reset script

---
+++ configuration/reset.sql
@@ -0,0 +1,8 @@
+-- Databricks notebook source
+DROP CATALOG IF EXISTS marci_dev CASCADE
+
+-- COMMAND ----------
+
+-- MAGIC %fs
+-- MAGIC 
+-- MAGIC rm -r /user/marcell.leleszi@datapao.com/f1/_checkpoint
